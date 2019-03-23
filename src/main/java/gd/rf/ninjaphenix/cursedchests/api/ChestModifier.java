d4477b1ac811aa42fbdb14409ffb2f758e3b9018
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2019-03-23 02:22:54 +0000

    Changed return type to boolean

--- src/main/java/gd/rf/ninjaphenix/cursedchests/api/ChestModifier.java
+++ src/main/java/gd/rf/ninjaphenix/cursedchests/api/ChestModifier.java
@@ -17,7 +17,8 @@ public interface ChestModifier
 	 * @param itemUsageContext
 	 * @param mainBlockPos The block pos of the single or bottom chest.
 	 * @param topBlockPos The block pos of the top chest block or null if the chest is a single chest.
+	 * @return true if the chest was modified otherwise false
 	 * @since 1.0.5
 	 */
-	void useOnChest(ItemUsageContext itemUsageContext, BlockPos mainBlockPos, @Nullable BlockPos topBlockPos);
+	boolean useOnChest(ItemUsageContext itemUsageContext, BlockPos mainBlockPos, @Nullable BlockPos topBlockPos);
 }
