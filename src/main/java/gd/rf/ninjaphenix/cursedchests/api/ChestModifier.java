4f581316bc47255557b244270a05c7e2980b93da
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2019-03-23 02:20:07 +0000

    First api interface (ChestModifier)

---
+++ src/main/java/gd/rf/ninjaphenix/cursedchests/api/ChestModifier.java
@@ -0,0 +1,23 @@
+package gd.rf.ninjaphenix.cursedchests.api;
+
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.math.BlockPos;
+import javax.annotation.Nullable;
+
+/**
+ * @author      NinjaPhenix
+ * @version     1.0.5
+ * @since       1.0.5
+ */
+public interface ChestModifier
+{
+
+	/**
+	 * This method is called automatically when an Item implementing this interface is used on a cursed chest.
+	 * @param itemUsageContext
+	 * @param mainBlockPos The block pos of the single or bottom chest.
+	 * @param topBlockPos The block pos of the top chest block or null if the chest is a single chest.
+	 * @since 1.0.5
+	 */
+	void useOnChest(ItemUsageContext itemUsageContext, BlockPos mainBlockPos, @Nullable BlockPos topBlockPos);
+}
