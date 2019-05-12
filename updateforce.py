6006d6f5234500b1e14f5343963172c444293b09
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2019-05-12 11:15:02 +0100

    Loader + python file updates.

--- updateforce.py
+++ updateforce.py
@@ -2,7 +2,8 @@ from os import system
 
 if __name__ == "__main__":
     system("gradlew --stop")
-    system("gradlew cleanLoomBinaries cleanLoomMappings")
+    system("gradlew cleanLoomBinaries")
+    system("gradlew cleanLoomMappings")
     system("gradlew genSources")
     system("gradlew cleanIdea openIdea")
     input("Press any key to exit.")
