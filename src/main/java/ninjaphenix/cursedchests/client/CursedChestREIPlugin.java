92ff4a59a9e3b5a61b03ef5c485625dca991fa29
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2019-08-25 15:41:44 +0100

    minor stylistic changes to REI plugin

--- src/main/java/ninjaphenix/cursedchests/client/CursedChestREIPlugin.java
+++ src/main/java/ninjaphenix/cursedchests/client/CursedChestREIPlugin.java
@@ -1,7 +1,6 @@
 package ninjaphenix.cursedchests.client;
 
 import me.shedaniel.math.api.Rectangle;
-import me.shedaniel.rei.api.BaseBoundsHandler;
 import me.shedaniel.rei.api.DisplayHelper;
 import me.shedaniel.rei.api.plugins.REIPluginV0;
 import net.fabricmc.api.EnvType;
@@ -18,25 +17,20 @@ import java.util.ArrayList;
 public class CursedChestREIPlugin implements REIPluginV0
 {
     @Override
-    public SemanticVersion getMinimumVersion() throws VersionParsingException
-    {
-        return SemanticVersion.parse("3.0-pre");
-    }
+    public SemanticVersion getMinimumVersion() throws VersionParsingException { return SemanticVersion.parse("3.0-pre"); }
+
+    @Override
+    public Identifier getPluginIdentifier() { return new Identifier("cursedchests", "reiplugin"); }
 
     @Override
     public void registerBounds(DisplayHelper displayHelper)
     {
-        BaseBoundsHandler handler = displayHelper.getBaseBoundsHandler();
-        handler.registerExclusionZones(ScrollableScreen.class, isOnRightSide ->
+        displayHelper.getBaseBoundsHandler().registerExclusionZones(ScrollableScreen.class, isOnRightSide ->
         {
-            MinecraftClient client = MinecraftClient.getInstance();
-            ScrollableScreen screen = (ScrollableScreen) client.currentScreen;
+            ScrollableScreen screen = (ScrollableScreen) MinecraftClient.getInstance().currentScreen;
             ArrayList<Rectangle> rv = new ArrayList<>(1);
-            if (isOnRightSide && screen.hasScrollbar()) { rv.add(new Rectangle(screen.getLeft() + 172, screen.getTop(), 22, 132));}
+            if (isOnRightSide && screen.hasScrollbar()) rv.add(new Rectangle(screen.getLeft() + 172, screen.getTop(), 22, 132));
             return rv;
         });
     }
-
-    @Override
-    public Identifier getPluginIdentifier() { return new Identifier("cursedchests", "reiplugin"); }
 }
